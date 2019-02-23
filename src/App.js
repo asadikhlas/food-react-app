cf48f75eccd762c420535c98298f77484d020a8c
Asad Ikhlas <asadikhlas03@gmail.com>
2019-02-23 22:07:56 +0500

    commit

--- src/App.js
+++ src/App.js
@@ -1,16 +1,36 @@
-import React, { Component } from 'react';
-import './App.css';
-import {recipes} from './tempList';
-import RecipeList from './components/recipeList';
-import RecipeDetails from './components/recipeDetails';
-
+import React, { Component } from "react";
+import "./App.css";
+import { recipes } from "./tempList";
+import RecipeList from "./components/recipeList";
+import RecipeDetails from "./components/recipeDetails";
 
 class App extends Component {
+  state = {
+    recipes: [],
+    url:
+      "https://www.food2fork.com/api/search?key=7f3620973180a4b890021a6aec83e0da"
+  };
+  async getRecipes() {
+    try {
+      const data = await fetch(this.state.url);
+      const jsonData = await data.json();
+      this.setState({
+        recipes: jsonData.recipes
+      });
+    } catch (error) {
+      console.log(error);
+    }
+  }
+  componentDidMount() {
+    this.getRecipes();
+  }
+
   render() {
+    console.log(this.state.recipes);
     return (
       <React.Fragment>
-      <RecipeList/>
-      <RecipeDetails/>
+        <RecipeList />
+        <RecipeDetails />
       </React.Fragment>
     );
   }
