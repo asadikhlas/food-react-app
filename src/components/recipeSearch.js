41a845726cb13a1fe37eda5eb745ffd7b8978509
Asad Ikhlas <asadikhlas03@gmail.com>
2019-02-24 17:45:11 +0500

    work on search

--- src/components/recipeSearch.js
+++ src/components/recipeSearch.js
@@ -2,7 +2,7 @@ import React, { Component } from "react";
 
 export default class RecipeSearch extends Component {
   render() {
-    const {value,handleChange, handleSubmit} = this.props
+    const { value, handleChange, handleSubmit } = this.props;
     return (
       <React.Fragment>
         <div className="container">
@@ -18,21 +18,22 @@ export default class RecipeSearch extends Component {
                 </label>
                 <div className="input-group">
                   <input
-                  type="text"
-                  name="search"
-                  placeholder="chicken,onion,carrots"
-                  className="form-control"
-                  value={value}
-                  onChange={handleChange}
-
+                    type="text"
+                    name="search"
+                    placeholder="chicken,onion,carrots"
+                    className="form-control"
+                    value={value}
+                    onChange={handleChange}
                   />
                   <div className="input-group-append">
-                    <button type="submit" className="input-group-text bg-primary text-white">
-                      <i className="fas fa-search"/>
+                    <button
+                      type="submit"
+                      className="input-group-text bg-primary text-white"
+                    >
+                      <i className="fas fa-search" />
                     </button>
                   </div>
                 </div>
-
               </form>
             </div>
           </div>
